c522efe54568269be8f104d1e48d41c643269ffd
vobrik <vobriker@gmail.com>
2019-04-09 12:33:41 +0100

    Added bootstrap and bootswatch

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -18,15 +18,17 @@
 	//	alert(me.text());
 	//});
 
-	var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fas");
 
 	$("#sidebarToggle").on("click", function () {
 		$sidebarAndWrapper.toggleClass("hide-sidebar");
-		if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-			$(this).text("Show Sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
 		} else {
-			$(this).text("Hide Sidebar");
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
 		}
 	});
-
 })();
