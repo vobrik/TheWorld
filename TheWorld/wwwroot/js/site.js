373376973328c7e4d7278515de391eb00dad9b5d
vobrik <vobriker@gmail.com>
2019-05-26 19:49:31 +0100

    Modified Index and Contacts with bootstrap

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -18,17 +18,17 @@
 	//	alert(me.text());
 	//});
 
-    var $sidebarAndWrapper = $("#sidebar,#wrapper");
-    var $icon = $("#sidebarToggle i.fas");
+  var $sidebarAndWrapper = $("#sidebar,#wrapper");
+  var $icon = $("#sidebarToggle i.fas");
 
 	$("#sidebarToggle").on("click", function () {
 		$sidebarAndWrapper.toggleClass("hide-sidebar");
-        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $icon.removeClass("fa-angle-left");
-            $icon.addClass("fa-angle-right");
+      if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+				$icon.removeClass("fa-angle-left");
+				$icon.addClass("fa-angle-right");
 		} else {
-            $icon.addClass("fa-angle-left");
-            $icon.removeClass("fa-angle-right");
+        $icon.addClass("fa-angle-left");
+        $icon.removeClass("fa-angle-right");
 		}
 	});
 })();
